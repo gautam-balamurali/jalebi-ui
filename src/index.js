49a6092e0b5c4be59b57c2310c2dbce69d398445
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-09-13 17:35:10 +0530

    feat: miscellaneous

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { DocsContextProvider } from "./core/contexts/DocsContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <DocsContextProvider>
+        <App />
+      </DocsContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
